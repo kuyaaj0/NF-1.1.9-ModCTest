26aed1f6c8b1ec225e02770be40c003a6f382cb6
Jay <83687427+kuyaaj0@users.noreply.github.com>
2026-02-25 20:14:51 -0800

    added this for the latest backup for the features i added on the game

---
+++ source/options/groupData/SmoothGroup.hx
@@ -0,0 +1,27 @@
+package options.groupData;
+
+class SmoothGroup extends OptionCata
+{
+	public function new(X:Float, Y:Float, width:Float, height:Float)
+	{
+		super(X, Y, width, height);
+
+		var option:Option = new Option(this, 'Smooth Settings', TITLE);
+		addOption(option);
+
+        /////--Smooth Option--\\\\\
+
+		var option:Option = new Option(this, 'Smooth', TEXT);
+		addOption(option);
+
+		var option:Option = new Option(this, 'smoothHealth', BOOL);
+		addOption(option, true);
+
+		var option:Option = new Option(this, 'smoothScore', BOOL);
+		addOption(option, true);
+
+		var option:Option = new Option(this, 'commaSeperated', BOOL);
+		addOption(option, true);
+		changeHeight(0); //初始化真正的height
+	}
+}
